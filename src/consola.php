b20b48e40f85fea7ca36ce815b35d709318e8d60
JTorrus <jtorrusgalindo@gmail.com>
2018-02-19 19:20:42 +0100

    Style added to main page, Bootstrap + jQuery implemented

--- src/consola.php
+++ src/consola.php
@@ -1,1 +1,39 @@
-<?php
+<?php ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>LSConsole</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
+          integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+<form action="executa.php" method="post">
+    <main class="container">
+        <section>
+            <h2>LSConsole - Simulació de terminal amb Php i Bootstrap</h2>
+            <p>
+                <span> Pressiona 'help' + <kbd>Enter</kbd> per veure les comandes disponibles</span>
+            </p>
+        </section>
+
+        <span class="command">
+            dani&javi:/$ <input type="text">
+        </span>
+    </main>
+</form>
+
+<script
+        src="http://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"
+        integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn"
+        crossorigin="anonymous"></script>
+</body>
+</html>
