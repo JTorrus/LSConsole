4a4af5abb21ef9fd99efe8db3715b0645389a042
dementedapple5 <daniel271196@gmail.com>
2018-02-20 21:46:39 +0100

    Funciones de los archivos .inc añadidas

--- src/executa.php
+++ src/executa.php
@@ -1,1 +1,16 @@
 <?php
+
+include '../include/sistema.inc';
+include '../include/directoris.inc';
+include '../include/arxius.inc';
+
+
+//Directoris.inc Functions
+
+function crea_directori($dir){
+    if ($dir != null){
+        mkdir($dir);
+    }else {
+        echo "Directory name is empty";
+    }
+}
