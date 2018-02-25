548b8d9f165bc9e376b75648ba981498ad25890a
dementedapple5 <daniel271196@gmail.com>
2018-02-25 02:09:06 +0100

    added functionality to all methods in directoris.inc and also already implmented them in executa.php

--- src/executa.php
+++ src/executa.php
@@ -12,12 +12,20 @@ switch ($arrInput[0]) {
         echo 'hola';
         break;
     case 'mkdir':
+        crea_directori($arrInput[1]);
         break;
     case 'rm':
+        esborra_directori($arrInput[1]);
         break;
     case 'mv':
+        mou_directori($arrInput[1], $arrInput[2]);
         break;
     case 'cp':
+        if (is_dir($arrInput[1]) && is_dir($arrInput[2])) {
+            copia_directori($arrInput[1], $arrInput[2]);
+        } elseif (is_file($arrInput[1]) && is_dir($arrInput[2])) {
+            copia_fitxer($arrInput[1], $arrInput[2]);
+        }
         break;
     case 'find':
         break;
@@ -30,8 +38,10 @@ switch ($arrInput[0]) {
     case 'md5':
         break;
     case 'ls':
+        llistat($arrInput[1]);
         break;
     case 'pwd':
+        ruta();
         break;
     case 'free':
         break;
